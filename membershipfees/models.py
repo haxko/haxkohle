3d3a2e0347052ef63af90ed381d90da3f5da91fa
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2020-06-21 10:43:48 +0200

    Add correct sign to a transaction amount

--- membershipfees/models.py
+++ membershipfees/models.py
@@ -4,6 +4,17 @@ from xmltodict import parse as parsexml
 import abc
 
 
+# ToDo: Replace ValueStore with a Transaction_History
+class ValueStore(models.Model):
+    key = models.CharField(max_length=128)
+    value = models.TextField()
+
+    def getValue(key):
+        return self.objects.get(key=key).first()
+
+    def setValue(key, value):
+        self.objects.create(key=key, value=value)
+
 class BankAccount(models.Model):
     owner = models.CharField(max_length=50)
     iban = models.CharField(max_length=32)
@@ -33,18 +44,20 @@ class BankDataDocument(models.Model):
         """Returns a list of transactions"""
         return
 
+    @abc.abstractmethod
     def is_valid(self):
-        """ Checks if the document is valid and checks wether the bank account has a valid state before and after the transactions are imported. """
-        transactions = self.get_transactions()
-        amount = transactions().reduce(lambda t1, t2: t1.amount + t2.amount)
-
-        if self.objects.count() >= 0:
-
+        """
+        Checks if the document is valid and checks wether the bank account
+        has a valid state before and after the transactions are imported.
+        """
+        return
 
     def save(self, *args, **kwargs):
         """ Prevents the Document from beeing saved. But saves the children instead. """
         for transaction in self.get_transactions():
             if self.is_valid() and transaction.is_valid():
+
+                ValueStore.set('last_account_balance', )
                 transaction.save(*args, **kwargs)
             else:
                 transaction.delete()
@@ -60,8 +73,25 @@ class CamtDocument(BankDataDocument):
                 self.data = parsexml(camt.read())['Document']['BkToCstmrAcctRpt']['Rpt']
         return self.data
 
-    def get_transactions(self):
+    def is_valid(self):
+        transactions = self.get_transactions()
+        transaction_amount = transactions().reduce(lambda t1, t2: t1.amount + t2.amount)
+        for balance in self.parse_data()['Bal']:
+            status = balance['Tp']['CdOrPrtry']['Cd']
 
+            if status in ['‘PRCD’', 'OPBD']:
+                start_balance = balance['Amt']['#text']
+                start_currency = balance['Amt']['@Ccy']
+            else if status in ['CLBD', 'CLAV']:
+                end_balance = balance['Amt']['#text']
+                end_currency = balance['Amt']['@Ccy']
+
+        if ValueStore.objects.get('last_account_balance').first() == start_balance: return False
+        if start_balance + transaction_amount == end_balance: return False
+
+        return True
+
+    def get_transactions(self):
         # Seperated in subfunction - may be useful for later cacheing.
         def get_or_create_transaction_from_ntry(ntry):
             bank_account, _ = BankAccount.objects.get_or_create(
@@ -70,8 +100,9 @@ class CamtDocument(BankDataDocument):
                 defaults = {'owner': ntry['Cdtr']['Nm']}
             )
 
+            sign = '-' if ntry['CdtDbtInd'] == 'DBIT' else ''
             transaction, _ = BankTransaction.objects.get_or_create(
-                amount = ntry['Amt']['#text'].strip(),
+                amount = float(sign + ntry['Amt']['#text'].strip()),
                 currency =  ntry['Amt']['@Ccy'].strip(),
                 booking_date = ntry['BookDt']['Dt'].strip(),
                 details = re.sub(r"\s+", " ", ntry['NtryDtls']['TxDtls']['RmtInf']['Ustrd'].strip().replace("\n", " ")),
