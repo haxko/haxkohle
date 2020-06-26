f1f6fccb5bdbd64c4850eb8c761d3dff595fedd0
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2020-06-26 23:23:24 +0200

    Add Template for admin-index

--- membershipfees/admin.py
+++ membershipfees/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Member, Subscription, BankAccount, BankTransaction
 
+
+admin.site.index_template = 'admin/index.html'
+
 admin.site.site_header = "Haxkohle Admin"
 admin.site.site_title = "Haxkohle"
 admin.site.index_title = "Haxkohle"
