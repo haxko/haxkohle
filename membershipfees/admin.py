349a4a0b7aff995c3cc1345ed7c22e4bf870bddc
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2020-06-27 17:12:12 +0200

    Change admin-views to be class based

--- membershipfees/admin.py
+++ membershipfees/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.shortcuts import render, redirect
 from .models import Subscription, BankAccount, BankTransaction
 
+from django.contrib.auth import forms as auth_forms, logout as auth_logout, login as auth_login, authenticate
+from django.views import generic as generic_views, View
+from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import user_passes_test
 
 admin.site.site_header = "Haxkohle Admin"
@@ -18,10 +21,34 @@ class BankTransactionAdmin(admin.ModelAdmin):
     list_display=('id', 'amount', 'currency', 'member', 'booking_date')
 admin.site.register(BankTransaction, BankTransactionAdmin)
 
-@user_passes_test(lambda user: user.is_active and user.is_superuser, '/admin/')
-def import_camt(request):
-    return render(request, 'admin/import_camt.html', {})
 
-@user_passes_test(lambda user: user.is_active and user.is_superuser, '/admin/')
-def match_transactions(request):
-    return render(request, 'admin/match_transactions.html', {})
+
+@method_decorator(user_passes_test(lambda user: user.is_active and user.is_superuser, '/admin/'), name='dispatch')
+class ImportCamt(generic_views.FormView):
+    template_name = 'admin/import_camt.html'
+    form_class = auth_forms.UserCreationForm
+    redirect_to = '/'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form_class})
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        return render(request, self.template_name, {'form': form})
+
+
+@method_decorator(user_passes_test(lambda user: user.is_active and user.is_superuser, '/admin/'), name='dispatch')
+class MatchTransactions(generic_views.FormView):
+    template_name = 'admin/match_transactions.html'
+    form_class = auth_forms.UserCreationForm
+    redirect_to = '/'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form_class})
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        return render(request, self.template_name, {'form': form})
+
--- membershipfees/urls.py
+++ membershipfees/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from membershipfees import views
-from .admin import match_transactions, import_camt
+from .admin import MatchTransactions, ImportCamt
 
 urlpatterns = [
-    path('admin/match_transactions/', match_transactions),
-    path('admin/import_camt/', import_camt),
+    path('admin/match_transactions/', MatchTransactions.as_view()),
+    path('admin/import_camt/', ImportCamt.as_view()),
     path('me/', views.user_status, name='membershipfees_me'),
     path('members/', views.members_status, name='membershipfees_members'),
     path('sections/', views.home, name='membershipfees_sections'),
