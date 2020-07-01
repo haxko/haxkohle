c91f8b6e85ce279bb039d63029105ff759c78c18
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2020-07-01 23:38:53 +0200

    Correct upload-password validation

--- membershipfees/forms.py
+++ membershipfees/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
 def validate_camt_upload_pass(value):
-    value_hash = hashlib.sha1(value.encode('utf-8')).hexdigest()
+    value_hash = hashlib.sha224(value.encode('utf-8')).hexdigest()
     if value_hash != settings.CAMT_UPLOAD_PASS:
         raise ValidationError(
             _('The upload password is incorrect.'),
